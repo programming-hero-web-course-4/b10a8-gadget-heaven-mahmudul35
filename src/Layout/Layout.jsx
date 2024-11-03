52d70fbceec4c3ccd387e9e45a04a5f36899e18a
mahmudul35 <101060541+mahmudul35@users.noreply.github.com>
2024-11-03 21:14:14 +0600

    design navbar

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 import Footer from "../component/Footer/Footer";
-
+import Navbar from "../component/Navbar/Navbar";
 const Layout = () => {
   return (
     <div>
-      <h1>Navbar</h1>
-      <div className="min-h-[calc(100vh-244px)]"></div>
+      <Navbar />
+      <div className="min-h-[calc(100vh-244px)]">
+        <Outlet />
+      </div>
       <Footer />
     </div>
   );
---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { CiHeart } from "react-icons/ci";
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100 justify-around items-center">
+      <div>
+        <a className="btn btn-ghost text-xl">Gadget Heaven</a>
+      </div>
+
+      <div>
+        <ul className="flex ">
+          <li>
+            {" "}
+            <a className="btn btn-ghost">Home</a>
+          </li>
+          <li>
+            {" "}
+            <a className="btn btn-ghost">Statistics</a>
+          </li>
+          <li>
+            {" "}
+            <a className="btn btn-ghost">Dashboard</a>
+          </li>
+        </ul>
+      </div>
+      <div className="flex-none">
+        <div>
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+            <div className="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+              </svg>
+              <span className="badge badge-sm indicator-item">8</span>
+            </div>
+          </div>
+        </div>
+        <div>
+          <p>
+            <CiHeart className="text-2xl" />
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
